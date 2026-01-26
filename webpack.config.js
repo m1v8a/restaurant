27e166a86de14cc9ec43484ca0a877a878593cfa
m1v8a <245783116+m1v8a@users.noreply.github.com>
2026-01-26 15:09:32 +0800

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+    mode: "development",
+    entry: "./src/app.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(import.meta.dirname, "dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/index.html"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|webp|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+}
